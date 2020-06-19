5f53e076ade59f729a71228eff42da02098ad2db
Cong Minh Nguyen <ngcongminh.dev@gmail.com>
2020-06-19 11:37:27 +0200

    only provide data for download/play when something was recorded

--- src/components/recordingTool.jsx
+++ src/components/recordingTool.jsx
@@ -12,16 +12,24 @@ class RecordingTool extends Component {
     constructor(props) {
         super(props);
 
-        this.started = false;
         let chunks = [];
 
         this.destination = this.props.streamDestination;
 
         this.mediaRecorder = new MediaRecorder(this.destination.stream);
         this.mediaRecorder.ondataavailable = function (evt) {
+            let data = evt.data
+            if (data.size == 0) {
+                return;
+            }
             chunks.push(evt.data);
         };
         this.mediaRecorder.onstop = function (evt) {
+            // in case there was nothing to record
+            if (chunks.length == 0) {
+                return;
+            }
+
             let blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
             let url = URL.createObjectURL(blob);
 
@@ -39,14 +47,14 @@ class RecordingTool extends Component {
 
     handleButtonClicked = function () {
         let isRecordingState = !this.state.isRecording;
-        this.setState({ isRecording: isRecordingState });
+        this.setState({isRecording: isRecordingState});
 
         if (isRecordingState) {
             this.mediaRecorder.start();
-            this.setState({ audioPlayerVisibility: "none" });
+            this.setState({audioPlayerVisibility: "none"});
         } else {
             this.mediaRecorder.stop();
-            this.setState({ audioPlayerVisibility: "inline" });
+            this.setState({audioPlayerVisibility: "inline"});
         }
     }
 
@@ -59,33 +67,38 @@ class RecordingTool extends Component {
         }
         let recordButton = null;
         let downloadVisibility = "invisible";//the download button is only visible if the recording is done
-        if(this.state.isRecording){
-            recordButton = <button className="btn btn-dark mb-2" type="button" id="recordButton"
-            onClick={this.handleButtonClicked}>Stop
-                </button>;
-        }else{
-            recordButton = <button className="btn btn-danger mb-2" type="button" id="recordButton"
-            onClick={this.handleButtonClicked}>Start
-                </button>;
-            if(this.state.audioPlayerVisibility == "inline"){//if audio player visible
+        if (this.state.isRecording) {
+            recordButton = <button
+                className="btn btn-dark mb-2" type="button" id="recordButton"
+                onClick={this.handleButtonClicked}>
+                Stop
+            </button>;
+        } else {
+            recordButton = <button
+                className="btn btn-danger mb-2" type="button" id="recordButton"
+                onClick={this.handleButtonClicked}>
+                Start
+            </button>;
+            if (this.state.audioPlayerVisibility == "inline") {//if audio player visible
                 downloadVisibility = "visible";
             }
         }
 
         return (<React.Fragment>
             <div class="shadow p-3 mt-2 mb-5 bg-light rounded">
-            <h4 style={hStyle}>RecordingTool</h4>
-            <button class=" btn btn-light ml-1 mb-2" style={recordingButtonStyle} onClick={() => this.props.onToolDelete("RecordingTool")}>
-            <Octicon icon={Dash}/></button>
-            <br></br>
-            {recordButton}
-            <a id="downloadLink" className={downloadVisibility}>
-            <button className="btn btn-secondary ml-2 mb-2" type="button">Download</button>
-            </a>
-            <br></br>
-            <audio controls id="audio" style={{display: this.state.audioPlayerVisibility}}></audio>
+                <h4 style={hStyle}>RecordingTool</h4>
+                <button class=" btn btn-light ml-1 mb-2" style={recordingButtonStyle}
+                        onClick={() => this.props.onToolDelete("RecordingTool")}>
+                    <Octicon icon={Dash}/></button>
+                <br></br>
+                {recordButton}
+                <a id="downloadLink" className={downloadVisibility}>
+                    <button className="btn btn-secondary ml-2 mb-2" type="button">Download</button>
+                </a>
+                <br></br>
+                <audio controls id="audio" style={{display: this.state.audioPlayerVisibility}}></audio>
             </div>
-            </React.Fragment>);
+        </React.Fragment>);
     }
 
 }
